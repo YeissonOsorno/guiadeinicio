65a31094c5c0967c7c5bb9e2479580113b926c0a
yeisson <yeisson.osorno@talent.com>
2022-08-20 10:46:38 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import {Route,Routes,Link} from 'react-router-dom';
-import Home from './pages/Home';
+import {Route,Routes} from 'react-router-dom';
+// import Home from './pages/Home';
 import FirstSteps from './pages/FirstSteps';
 
 export default function App() {
   return (
     <>     
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/'  element={<FirstSteps/>}/>
         <Route path='/primerospasos' element={<FirstSteps/>}/>
       </Routes>
     </>
