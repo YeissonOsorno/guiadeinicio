984257281688b6f80cdd3224c43f23e5b5d9ab26
yeisson <yeisson.osorno@talent.com>
2022-08-20 10:23:43 -0500

    initial page primeros pasos

---
+++ src/pages/FirstSteps.js
@@ -0,0 +1,144 @@
+ import React from 'react'
+
+ const FirstSteps = () => {
+    
+    return ( 
+        <div className='background bg-fixed bg-gray-700 bg-opacity-50'>
+
+            {/* Primeros pasos video bienvenida */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-1 ">
+                <div className='py-5 ml-12 md:py-10 md:ml-10 justify-items-center'>
+                    <p className=' flex text-3xl md:text-5xl font-extrabold md:font-bold text-white'>MIS PRIMEROS PASOS</p>                   
+                </div> 
+                <div className='relative  overflow-auto p-8'>                
+                    <iframe className='w-full aspect-auto rounded-lg shadow-lg' src="https://player.vimeo.com/video/734924234" title='1'  frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>      
+            </div>
+
+            <div className='grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-1 ml-5 md:ml-0'>
+               <div className='py-5 md:ml-0 justify-items-end'>
+                <p className='flex text-3xl md:text-5xl font-semibold text-yellow-600'>NO SABES CÓMO INICIAR? AQUÍ TE AYUDAMOS</p>
+               </div>
+            </div>
+
+            {/* Crear cuenta en deriv */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-2 justify-between ">
+                <div className='py-5 ml-16 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>
+                        Cómo crear una cuenta en Deriv. Puedes registrarte haciendo click 
+                    <a className='text-red-700' target="_blank" rel="noreferrer" href='https://deriv.com/signup'> aquí.</a>  </p>                   
+                </div> 
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949552" title='2'  frameborder="0" allow="autoplay; fullscreen" allowfullscreen>                    
+                    </iframe>
+                </div>     
+            </div>
+
+            {/* Crear cuenta demo y real para conectar con MT5 */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-2 justify-between ">
+                    
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949552" title='3' frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>        
+                <div className='py-5 ml-16 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>
+                    Crear cuenta Demo y Real para operar con MT5</p>                   
+                </div> 
+                         
+            </div>
+
+            {/* Conectar deriv */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-3 justify-between ">
+                <div className='py-5 ml-16 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>Conectar cuenta Deriv con MT5  </p>
+                    <p className='text-xl md:text-xl font-normal justify-center text-white p-2'>iPhone & Android  </p>
+                </div>     
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949552"  title='4' frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>        
+                <div className='relative rounded-xl overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740946767" title='5' allow="autoplay; fullscreen" allowfullscreen>
+                        
+                    </iframe>
+                </div>            
+            </div>
+
+            {/* Tomar una operacion */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-3 justify-between ">
+                <div className='py-5 ml-16 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>Tomar una operación desde MT5  </p>
+                    <p className='text-xl md:text-xl font-normal justify-center text-white p-2'>iPhone & Android  </p>
+                </div>     
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949515" title='6' frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>        
+                <div className='relative rounded-xl overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740946785" title='7' allow="autoplay; fullscreen" allowfullscreen>
+                        
+                    </iframe>
+                </div> 
+            </div>
+
+            {/* Trazar SL y TP */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-3 justify-between ">
+                <div className='py-5 ml-16 md:py-36 md:ml-10  '>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>Como trazar SL & TP en MT5  </p>
+                    <p className='text-xl md:text-xl font-normal justify-center text-white p-2'>iPhone & Android  </p>
+                </div>     
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949515" title='8' frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>        
+                <div className='relative rounded-xl overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740946785" title='9' allow="autoplay; fullscreen" allowfullscreen>
+                        
+                    </iframe>
+                </div> 
+            </div>
+
+            {/* Cerrar parciales */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-3 justify-between ">
+                <div className='py-5 ml-16 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>Como cerrar parciales en MT5  </p>
+                    <p className='text-xl md:text-xl font-normal justify-center text-white p-2'>iPhone & Android  </p>
+                </div>     
+                <div className='relative rounded-full overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740949515" title='10' frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
+                    
+                    </iframe>
+                </div>        
+                <div className='relative rounded-xl overflow-auto p-8'>                
+                    <iframe className='w-full aspect-square rounded-lg shadow-lg' src="https://player.vimeo.com/video/740946785" title='11' allow="autoplay; fullscreen" allowfullscreen>
+                        
+                    </iframe>
+                </div> 
+            </div>
+
+            {/* Descargar guia plan 7 semanas */}
+            <div class="grid grid-rows-1 md:grid-rows-1 grid-cols-1 md:grid-cols-2 justify-items-center mb-0 ">
+                <div className='py-5 ml-10 md:py-36 md:ml-10'>
+                    <p className='text-3xl md:text-5xl font-semibold justify-center text-white p-2'>Te dejamos una guia para empezar a estudiar   </p>
+                    <p className='text-xl md:text-xl font-normal justify-center text-yellow-600 p-2'>presiona el boton de descarga  </p>
+                </div>     
+                <div className='sm:p-8  md:py-56 md:ml-36 '>                
+                    <a href='https://drive.google.com/file/d/1qZTqeyWuFaZHlEFCGug21gnU7RZqpeGN/view?usp=sharing' rel="noreferrer" target="_blank" 
+                    className='btn-custom btn-primary text-white   rounded-lg '>Descarga tú guia aquí</a>
+                </div>
+            </div>
+
+        </div>
+        
+      
+     );
+ }
+  
+ export default FirstSteps;
